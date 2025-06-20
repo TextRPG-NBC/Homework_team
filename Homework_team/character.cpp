2a2dd43bc4b4db21f675e233aaa74e58a164aee1
BannedUser0 <vvmyu13@gmail.com>
2025-06-20 13:20:54 +0900

    update_character
    fix character

--- Homework_team/character.cpp
+++ Homework_team/character.cpp
@@ -1,48 +1,33 @@
+#include <string>
+#include <vector>
 #include <iostream>
 #include "character.h"
-
 using namespace std;
 
-Character::Character(const string& name, int level, int exp, int money)
-    : name(name), level(level), exp(exp),
-    health(180 + (level * 20)),
-    attackPower(25 + (level * 5)),
-    criticalRate(0),  // 필요하면 초기화
-    money(money) {
+Character::Character(const string& name, int level, int exp, int criticalRate, int money, vector<string> invenroty)
+	: name(name), level(level), exp(exp), health(180 + (level * 20)), attackPower(25 + (level * 5)), criticalRate(criticalRate), money(money), inventory(inventory) {
 }
 
-void Character::displayStatus() const {
-    cout << "Name: " << name << endl;
-    cout << "Level: " << level << endl;
-    cout << "Experience: " << exp << endl;
-    cout << "Health: " << health << endl;
-    cout << "Attack Power: " << attackPower << endl;
-    cout << "Critical Rate: " << criticalRate << "%" << endl;
-    cout << "Money: " << money << endl;
-    cout << "Inventory: ";
-    for (const auto& item : inventory) {
-        cout << item << " ";
-    }
-    cout << endl;
+void Character::displayInfo() const {
+	cout << "Name: " << name << endl;
+	cout << "Level: " << level << endl;
+	cout << "Experience: " << exp << endl;
+	cout << "Health: " << health << endl;
+	cout << "Attack Power: " << attackPower << endl;
+	cout << "Critical Rate: " << criticalRate << "%" << endl;
+	cout << "Have money : " << money << endl;
+	cout << "Inventory: " << endl;
+	cout << "------------------------" << endl;
 }
 
-void Character::levelUp() {
-    level++;
-    health = 180 + (level * 20);
-    attackPower = 25 + (level * 5);
-    cout << name << " leveled up to " << level << "!" << endl;
+void Character::levelUp()
+{
 }
 
-void Character::useItem(int index) {
-    if (index >= 0 && index < inventory.size()) {
-        cout << name << " used " << inventory[index] << "." << endl;
-        inventory.erase(inventory.begin() + index);
-    }
-    else {
-        cout << "Invalid item index." << endl;
-    }
+void Character::useItem()
+{
 }
 
-void Character::visitShop() {
-    cout << "Visiting the shop..." << endl;
+void Character::visitShop()
+{
 }
