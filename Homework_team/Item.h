8779cc07a0c4d42e59f63f2e37872017ebe7cce1
BannedUser0 <vvmyu13@gmail.com>
2025-06-22 16:43:16 +0900

    init item
    new item

--- Homework_team/Item.h
+++ Homework_team/Item.h
@@ -1,6 +1,23 @@
 #pragma once
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 class Item
 {
-	//test
+	string itemName;
+	int price;
+	int attackPower_attribute;
+	int health_attribute;
+	int critRate_attribute;
+
+public:
+
 };
 
+class equpItem : public Item
+{
+
+};
