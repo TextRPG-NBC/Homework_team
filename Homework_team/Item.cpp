a4b4c035580b9d18334f7c6b24f394c38325b1fb
BannedUser0 <vvmyu13@gmail.com>
2025-06-23 15:46:39 +0900

    Update(character, item)
    new feature character, item

--- Homework_team/Item.cpp
+++ Homework_team/Item.cpp
@@ -6,28 +6,53 @@
 
 using namespace std;
 
-Item::Item(string name, int price, int attackPower, int health, int critRate)
-	: itemName(name), price(price), attackPower_attribute(attackPower), health_attribute(health), critRate_attribute(critRate) {
+Item::Item(string name, int itemID, int price, int attackPower, int health, int critRate)
+	: itemName("name")
+	, itemID(0)
+	, price(0)
+	, attackPower_attribute(0)
+	, health_attribute(0)
+	, critRate_attribute(0) {};
+
+
+class DamageWeapon : public Item //아이템 상속 : 무기
+{
+public:
+	DamageWeapon() : Item("DamageWeapon", 1, 150, 40, 0, 0) {}; // 순서 : 이름, 아이템ID(0:무기, 1:방어구, 2:포션), 가격, 공격력, 체력, 크리티컬 확률
+};
+
+class CritWeapon : public Item //아이템 상속 : 무기
+{
+public:
+	CritWeapon() : Item("DamageWeapon", 1, 140, 10, 0, 30) {}; // 순서 : 이름, 아이템ID(0:무기, 1:방어구, 2:포션), 가격, 공격력, 체력, 크리티컬 확률
+};
+
+class Amour : public Item //아이템 상속 : 방어구
+{
+public:
+	Amour() : Item("Amour", 1, 110, 0, 150, 0) {}; // 순서 : 이름, 아이템ID(0:무기, 1:방어구, 2:포션), 가격, 공격력, 체력, 크리티컬 확률
 };
 
 class healthPotion : public Item //아이템 상속 : 체력포션
 {
 public:
-	healthPotion() : Item("Health Potion", 50, 0, 50, 0) {}; // 순서 : 이름, 가격, 공격력, 체력, 크리티컬 확률
+	healthPotion() : Item("Health Potion", 3, 50, 0, 50, 0) {}; // 순서 : 이름, 아이템ID(0:무기, 1:방어구, 2:포션), 가격, 공격력, 체력, 크리티컬 확률
 };
 
 class powerPotion : public Item //아이템 상속 : 데미지 증가 포션
 {
 public:
-	powerPotion() : Item("Power Potion", 50, 10, 0, 0) {}; // 순서 : 이름, 가격, 공격력, 체력, 크리티컬 확률
+	powerPotion() : Item("Power Potion", 3, 50, 10, 0, 0) {}; // 순서 : 이름, 아이템ID(0:무기, 1:방어구, 2:포션), 가격, 공격력, 체력, 크리티컬 확률
 };
 
 class CritPotion : public Item //아이템 상속 : 크리티컬 확률 증가 포션
 {
 public:
-	CritPotion() : Item("Crit Potion", 50, 0, 0, 10) {}; // 아이템 이름, 가격, 공격력, 체력, 크리티컬 확률
+	CritPotion() : Item("Crit Potion", 3, 50, 0, 0, 10) {}; // 순서 : 이름, 아이템ID(0:무기, 1:방어구, 2:포션), 가격, 공격력, 체력, 크리티컬 확률
 		
 };
+
+
 
 //	case Item::healthPotion: // 아이템 종류에 따라 객체 생성??
 //	return new healthPotion();
--- Homework_team/Item.h
+++ Homework_team/Item.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "CommonInclude.h"
 #include <string>
 #include <iostream>
 
@@ -9,14 +10,19 @@ class Item
 {
 public:
 	string itemName;
+	int itemID;			//0은 무기, 1은 방어구, 2는 포션
 	int price;
 	int attackPower_attribute;
 	int health_attribute;
 	int critRate_attribute;
 
-	Item(string name, int price, int attackPower, int health, int critRate)
-		: itemName(name), price(price), attackPower_attribute(attackPower), health_attribute(health), critRate_attribute(critRate) {
-	}
+	Item(string name, int itemID, int price, int attackPower, int health, int critRate)
+		: itemName(name)
+		, itemID(0)
+		, price(price)
+		, attackPower_attribute(attackPower)
+		, health_attribute(health)
+		, critRate_attribute(critRate) {}
 
 	virtual ~Item() {};
 };
