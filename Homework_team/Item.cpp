3eb50d9d06fecb4576bb049e9830f390c4d1e268
BannedUser0 <vvmyu13@gmail.com>
2025-06-23 12:21:49 +0900

    update item
    update item

--- Homework_team/Item.cpp
+++ Homework_team/Item.cpp
@@ -1,33 +1,19 @@
-
+// PotionItems.h
 #pragma once
-
 #include "Item.h"
-#include <string>
 
-using namespace std;
-
-Item::Item(string name, int price, int attackPower, int health, int critRate)
-	: itemName(name), price(price), attackPower_attribute(attackPower), health_attribute(health), critRate_attribute(critRate) {
-};
-
-class healthPotion : public Item //아이템 상속 : 체력포션
-{
+class healthPotion : public Item {
 public:
-	healthPotion() : Item("Health Potion", 50, 0, 50, 0) {}; // 순서 : 이름, 가격, 공격력, 체력, 크리티컬 확률
+    healthPotion() : Item("Health Potion", 50, 0, 50, 0) {}
 };
 
-class powerPotion : public Item //아이템 상속 : 데미지 증가 포션
-{
+class powerPotion : public Item {
 public:
-	powerPotion() : Item("Power Potion", 50, 10, 0, 0) {}; // 순서 : 이름, 가격, 공격력, 체력, 크리티컬 확률
+    powerPotion() : Item("Power Potion", 50, 10, 0, 0) {}
 };
 
-class CritPotion : public Item //아이템 상속 : 크리티컬 확률 증가 포션
-{
+class critPotion : public Item {
 public:
-	CritPotion() : Item("Crit Potion", 50, 0, 0, 10) {}; // 아이템 이름, 가격, 공격력, 체력, 크리티컬 확률
-		
+    critPotion() : Item("Crit Potion", 50, 0, 0, 10) {}
 };
 
-//	case Item::healthPotion: // 아이템 종류에 따라 객체 생성??
-//	return new healthPotion();
--- Homework_team/Item.h
+++ Homework_team/Item.h
@@ -1,27 +1,39 @@
+// Item.h
 #pragma once
-
 #include <string>
 #include <iostream>
-
 using namespace std;
 
-class Item
-{
+class Item {
+private:
+    string itemName;
+    int price;
+    int attackPower_attribute;
+    int health_attribute;
+    int critRate_attribute;
+
 public:
-	string itemName;
-	int price;
-	int attackPower_attribute;
-	int health_attribute;
-	int critRate_attribute;
+    // Constructor
+    Item(string name, int price, int attackPower, int health, int critRate)
+        : itemName(name)
+        , price(price)
+        , attackPower_attribute(attackPower)
+        , health_attribute(health)
+        , critRate_attribute(critRate) {}
 
-	Item(string name, int price, int attackPower, int health, int critRate)
-		: itemName(name), price(price), attackPower_attribute(attackPower), health_attribute(health), critRate_attribute(critRate) {
-	}
+    virtual ~Item() {}
 
-	virtual ~Item() {};
-};
+    // Getter
+    string getItemName() const { return itemName; }
+    int getPrice() const { return price; }
+    int getAttackPower() const { return attackPower_attribute; }
+    int getHealth() const { return health_attribute; }
+    int getCritRate() const { return critRate_attribute; }
 
-class equpItem : public Item
-{
-
+    // Setter
+    void setItemName(const string& name) { itemName = name; }
+    void setPrice(int p) { price = p; }
+    void setAttackPower(int attackPower) { attackPower_attribute = attackPower; }
+    void setHealth(int health) { health_attribute = health; }
+    void setCritRate(int critRate) { critRate_attribute = critRate; }
 };
